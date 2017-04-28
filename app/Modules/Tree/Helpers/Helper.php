f0f058b81364005a041392437e138d24d93715fc
Alex Khan <xannn94@mail.ru>
2017-04-28 14:16:11 +0600

    Initial commit

---
+++ app/Modules/Tree/Helpers/Helper.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Modules\Tree\Helpers;
+
+use Xannn94\Modules\Facades\Module;
+
+class Helper{
+
+    public static function getModulesSelect($all = false){
+        $modules = Module::all();
+        dd($modules);
+    }
+
+
+}
