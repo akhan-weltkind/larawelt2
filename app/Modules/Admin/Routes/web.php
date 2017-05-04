548c5fd6ded65a9f687959fd0c18bb6b14fd951c
akhan@weltkind.com <axiles94>
2017-05-04 13:08:43 +0600

    fix file and image item admin menu

--- app/Modules/Admin/Routes/web.php
+++ app/Modules/Admin/Routes/web.php
@@ -3,6 +3,6 @@
 Route::group(['prefix' => config('cms.uri')], function() {
     Route::get('/', 'HomeController@index');
 
-    Route::get('files/images', 'FilesController@images')->name('admin.files.images');
+    Route::get('files/images', 'ImagesController@images')->name('admin.files.images');
     Route::get('files/files', 'FilesController@files')->name('admin.files.files');
 });
